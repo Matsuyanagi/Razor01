78716ddbfe68f94545d4b37f3f2c8438d6fdf88c
Matsuyanagi <1475814+Matsuyanagi@users.noreply.github.com>
2018-06-24 18:05:29 +0900

    Add nuget RazorEngine

--- Razor01/Program.cs
+++ Razor01/Program.cs
@@ -1,15 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security;
+using System.Security.Permissions;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using RazorEngine;
+using RazorEngine.Templating;
 
 namespace Razor01
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+
+            if ( AppDomain.CurrentDomain.IsDefaultAppDomain() ) {
+                // RazorEngine cannot clean up from the default appdomain...
+                Console.WriteLine( "Switching to secound AppDomain, for RazorEngine..." );
+                AppDomainSetup adSetup = new AppDomainSetup();
+                adSetup.ApplicationBase = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+                var current = AppDomain.CurrentDomain;
+                // You only need to add strongnames when your appdomain is not a full trust environment.
+                var strongNames = new StrongName[0];
+
+                var domain = AppDomain.CreateDomain(
+                    "MyMainDomain", null,
+                    current.SetupInformation, new PermissionSet( PermissionState.Unrestricted ),
+                    strongNames );
+                var exitCode = domain.ExecuteAssembly( Assembly.GetExecutingAssembly().Location );
+                // RazorEngine will cleanup. 
+                AppDomain.Unload( domain );
+                return exitCode;
+            }
+
+            // Continue with your code.
+            string template = "Hello @Model.Name, welcome to RazorEngine!";
+            var result =
+                Engine.Razor.RunCompile( template, "templateKey", null, new { Name = "World" } );
+
+            Console.WriteLine( result );
+
+            return 0;
         }
     }
 }
